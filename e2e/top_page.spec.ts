901cc172023911c0b043350e3a89965c263fd00e
Shun E <shun-eto@sopherre.com>
2023-05-24 19:16:26 +0900

    修正

--- e2e/top_page.spec.ts
+++ e2e/top_page.spec.ts
@@ -1,10 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test.use({
-  viewport: {
-    height: 844,
-    width: 390,
-  },
+  viewport: { height: 844, width: 390 },
 });
 
 test.describe("TopPage", () => {
@@ -12,13 +9,7 @@ test.describe("TopPage", () => {
     await page.goto("./");
   });
 
-  test("transition to 404 page", async ({ page }) => {
-    await page.getByRole("link", { name: "test" }).click();
-    await expect(page).toHaveTitle("Not Found Page");
-    await expect(page.getByText("Not Found Page.")).toBeVisible();
-  });
-
-  test.only("test", async ({ page }) => {
+  test.only("top page to 404 page to top page.", async ({ page }) => {
     await expect(page).toHaveTitle("Top Page");
     await expect(page.getByRole("heading", { name: "Top Page" })).toBeVisible();
     await page.getByRole("button", { name: "to Sample Page" }).click();
@@ -34,14 +25,4 @@ test.describe("TopPage", () => {
 
     await expect(page).toHaveTitle("Top Page");
   });
-
-  // test("test", async ({ page }) => {
-  //   await page.goto("http://localhost:3001/");
-  //   await page.getByRole("heading", { name: "Top Page" }).click();
-  //   await page.getByRole("heading", { name: "Top Page" }).dblclick();
-  //   await page.getByRole("button", { name: "to Sample Page" }).click();
-  //   await page.getByRole("heading", { name: "Not Found Page" }).dblclick();
-  //   await page.getByRole("button", { name: "return to Top Page" }).click();
-  //   await page.getByRole("heading", { name: "Top Page" }).dblclick();
-  // });
 });
